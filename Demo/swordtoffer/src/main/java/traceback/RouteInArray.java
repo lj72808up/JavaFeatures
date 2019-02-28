a3bbf0e1d472f6a14c449905168c058ba1692e45
lj72808up <lj72808up@163.com>
2019-02-28 20:58:04 +0800

    sword to offer

--- Demo/swordtoffer/src/main/java/traceback/RouteInArray.java
+++ Demo/swordtoffer/src/main/java/traceback/RouteInArray.java
@@ -11,4 +11,25 @@ package traceback;
  * 这样的3 X 4 矩阵中包含一条字符串"bfce"的路径，但是矩阵中不包含"abfb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入该格子。
  */
 public class RouteInArray {
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str)
+    {
+        for (int i = 0; i < matrix.length; i++) {
+            if(matrix[i]==str[0]){
+                int row = i / 4;
+                int col = i % 4;
+                return findPath(matrix,row,col,str,1); // 寻找str[1]
+            }
+        }
+        return false;
+    }
+
+    private boolean findPath (char[] matrix, int rows, int cols, char[] str, int site){
+
+    }
+
+    private char getElem(char[] matrix, int row, int col){
+        if(row>2 || row<0 || col>3 || col<0)
+            return '\0';
+        return matrix[row*4+col];
+    }
 }
