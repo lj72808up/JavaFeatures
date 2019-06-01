01761804a728dbd75a2b771ee652f53910e67bfe
lj72808up <lj72808up@163.com>
2019-06-01 11:42:47 +0800

    scala fp

--- Scala/FP-Scala/src/main/scala/unit3/List.scala
+++ Scala/FP-Scala/src/main/scala/unit3/List.scala
@@ -133,6 +133,11 @@ object List{
   def length[A](l:List[A]):Int = {
     foldRight(l,0)((_,acc)=>acc+1)
   }
+  // 使用一种fold实现反转链表
+  def reverse[A](l:List[A]):List[A] = {
+    // 这里使用List[A()而不是Nil是必要的, 因为需要推断出类型
+    foldLeft(l,List[A]())((h,acc)=>Cons(h,acc))
+  }
 
   /**
     * 如上的foldRight不是尾递归,可能发生stack overflow 可以写一个尾递归的foldeft
@@ -144,9 +149,59 @@ object List{
     }
   }
   def sum3[A](l:List[Int]):Int = {
-    foldLeft(l,0)((x,y)=>{print(s"${x}+${y} ; ");x+y})
+    foldLeft(l,0)((x,y)=>{print(s"${y}+${x} ; ");x+y})
   }
-  //3.12
+  // 通过foldleft实现fpoldright, 避免栈溢出
+  def foldRightViaFoldLeft[A,B](l: List[A], init: B)(f: (A,B) => B): B ={
+    foldLeft(reverse(l), init)((a,b) => f(a,b))
+  }
+  // 用fold实现append两个list.遍历深度只取决于l1
+  def appendViaFold[A](l1:List[A],l2:List[A]):List[A] = {
+    List.foldRight(l1,l2)((elem,acc)=>Cons(elem,acc))
+  }
+
+  /**
+    * 3.4.1 更多操作函数
+    */
+  def add1(l:List[Int]):List[Int] = {
+    foldLeft(l,List[Int]())((ele,l)=>Cons(ele+1,l))
+  }
+
+  /**
+    * foldRight实现的map, 产出结果与原List顺序相同. 这是因为List也是右结合的, 本身就是一种递归结构
+    */
+
+  def map[A,B](l:List[A],f:A=>B):List[B] = {
+    foldRight(l,Nil:List[B])((ele,l)=>Cons(f(ele),l))
+  }
+  def filter[A](l:List[A])(f:(A)=>Boolean): List[A] = {
+    foldRight(l,List[A]())((head,tail)=> if(f(head)) Cons(head,tail) else tail)
+  }
+
+  /**
+    * append连接2个列表, concat连接多个列表
+    * @param l
+    * @tparam A
+    * @return
+    */
+  def concat[A](l:List[List[A]]):List[A] = {
+    foldRight(l,List[A]())(append)
+  }
+  // flatMap(List(1,2,3))(x=>List(x,x)) = Cons(1,Cons(1,Cons(2,Cons(2,Cons(3,Cons(3,Nil))))))
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] =
+    concat(map(l,f))
+
+  /**
+    * 使两个列表对应位置的元素相加, 产生新的列表:
+    * List(1,1,1),List(2,3,4) => List(3,4,5)
+    */
+  def addPairs(l1:List[Int],l2:List[Int]):List[Int] = {
+    (l1,l2) match {
+      case (_,Nil) => Nil
+      case (Nil,_) => Nil
+      case (Cons(h1,t1),Cons(h2,t2)) => Cons(h1+h2,addPairs(t1,t2))
+    }
+  }
 }
 
 
@@ -164,10 +219,19 @@ object Test{
     println(List.sum2(List(2,3,4)))
     println(List.length(List(1,2,99)))
     println(List.sum3(List(3,4,7)))
+    println(List.reverse(List(1,2,3,4)))
+    println(List.appendViaFold(List(1,2,3),List(4,5,6)))
+    println(List.add1(List(1,2,1)))
+    println(List.map(List(1,2,3,4),(x:Int)=>2*x))
+    println(List.filter(List(1,2,3,4))((x:Int)=>x%2==0))
+    println(List.concat(List(List(1,2),List(3,4))))
+    println(List.flatMap(List(1,2,3))(x=>List(x,x)))
+    println(List.addPairs(List(1,1,1),List(2,3,4)))
   }
 }
 
-/**
+/** tips:
   * 因为纯函数不能修改变量和产生副作用, 因此, scala中的集合都是不可变的.
-  * 比如ListA ++ ListB 产生一个新的ListC, 而这不会产生数据复制, 只会增加引用, 也因为scala集合是不可变的, 因此可以安全的共享数据而不会改变
+  * 比如ListA ++ ListB 产生一个新的ListC, 而这不会产生数据复制, 只会增加引用, 也因为scala集合是不可变的, 因此可以安全的共享数据而不必担心数据改变
+  * https://github.com/fpinscala/fpinscala/blob/master/answers/src/main/scala/fpinscala/datastructures/List.scala
   */
