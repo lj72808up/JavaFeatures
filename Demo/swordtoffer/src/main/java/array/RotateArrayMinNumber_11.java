f740327597a1c59789576c50cab57ea4c6e4acac
lj72808up <lj72808up@163.com>
2019-07-30 00:42:23 +0800

    reorder array

--- Demo/swordtoffer/src/main/java/array/RotateArrayMinNumber_11.java
+++ Demo/swordtoffer/src/main/java/array/RotateArrayMinNumber_11.java
@@ -6,30 +6,38 @@ package array;
  * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
  */
 public class RotateArrayMinNumber_11 {
-    public int minNumberInRotateArray(int [] array) {
-        int index1 = 0;
-        int index2 = array.length-1;
+    public int minNumberInRotateArray(int [] arr) {
+        if (arr.length==0)
+            return 0;
+        int index1=0;
+        int index2=arr.length-1;
+        int mid=index1;
 
-        while(index2-index1>1){   // mid划分两个数组, 前面是大数组,后面是rotation的小数组
-            int mid = (index1+index2) >> 1;
+        while(arr[index1]>=arr[index2]){
+            if (index2-index1==1){
+                mid = index2;
+                break;
+            }
 
-            if ((array[mid]==array[index1])&&(array[mid]==array[index2]))
-                return searchMin(array,index1,index2);
-
-            if(array[mid] >= array[index2]) {
-                index1 = mid;
-            }else if(array[mid] <= array[index1]){
-                index2 = mid;
+            mid = (index1+index2)>>1;
+            if (arr[index1]==arr[mid] && arr[index2]==arr[mid])
+                this.searchMin(arr,index1,index2);
+            if (arr[mid]>=arr[index1]){
+                index1=mid;
+            }else if(arr[mid]<=arr[index2]){
+                index2=mid;
             }
         }
-        //index2-index1=1时退出, index2是后面rotation小数组的头
-        return array[index2];
+
+        return arr[mid];
     }
-    private int searchMin(int[] array,int start,int end){
-        int min = array[start];
-        for(int i=start;i<=end;i++){
-            if (array[i]<min)
-                min = array[i];
+
+    private int searchMin(int [] arr,int start,int end){
+        int min = arr[start];
+        for (int i=start;i<=end;i++){
+            if (arr[i]<min){
+                min = arr[i];
+            }
         }
         return min;
     }
@@ -42,9 +50,13 @@ public class RotateArrayMinNumber_11 {
 }
 /**
  * 思路:
- *   旋转的数组有一个规律, 就是旋转后, 第1个元素, 一定大于等最后一个元素,因此若套用二分查找的思路:
+ *   旋转的数组有一个规律, 就是旋转后, 第1个元素, 一定大于等于最后一个元素,因此若套用二分查找的思路:
  *   先计算出mid=(index1+index2)>>1,
  *      若arr[mid]>=arr[index1], 说明mid属于前半段, 则旋转点一定是在mid和index2之间
  *      若arr[mid]<=arr[index2], 说明mid属于后半段, 则旋转点一定是在index1和mid之间
- *   此外, 若值 旋转了0个元素,即{1,2,3,4}这样依然属于旋转数组, 此时第一个元素
+ *   最后,当index1和index2相邻时, index2即为最小值
+ *   此外, 若只旋转了0个元素,即{1,2,3,4}这样依然属于旋转数组, 此时第一个元素小于第二个元素, 可直接找到最小值arr[0]
+ *   再次, 上述判断漏掉一个可能, 就是arr[index1]=arr[mid]=arr[index2]的情形, 该情形无法判断最小值在哪一段:
+ *      例如: [1,0,1,1,1]和[1,1,1,0,1]
+ *      因此, 只能通过顺序查找整个列表
  */
--- Demo/swordtoffer/src/main/java/binarytree/RebuildBinaryTree.java
+++ Demo/swordtoffer/src/main/java/binarytree/RebuildBinaryTree.java
@@ -1,7 +1,5 @@
 package binarytree;
 
-import apple.laf.JRSUIUtils;
-
 /**
  * 题目描述
  * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
